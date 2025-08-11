138af63b5af5ddb675ed9056fd56776c8d18c385
Mahek-H-Varasada <mahekvarasada7@gmail.com>
2025-08-11 22:35:32 +0530

    Update usbls_tx_addr_endp_inv.v

--- usbls_tx_addr_endp_inv.v
+++ usbls_tx_addr_endp_inv.v
@@ -1,28 +1,4 @@
-//-----------------------------------------------------------------------------
-// Company:         Vicharak Computers PVT LTD
-// Engineer:        Mahekkumar Varasada <mahekvarasada@gmail.com>
-// 
-// Create Date:     April 2, 2025
-// Design Name:     usbls_tx_addr_endp_inv
-// Module Name:     usbls_tx_addr_endp_inv.v
-// Project:         PeriPlex
-// Target Device:   Trion T120F324
-// Tool Versions:   Efinix Efinity 2024.2 
-// 
-// Description: 
-//    reverse the address,endpoint(11 bits) to endpoint,address  
-// 
-// Dependencies: 
-// 
-// Version:
-//    1.0 - 02/04/2025 - MKV - Initial release
-// 
-// Additional Comments: 
-//  
-// 
-// License: 
-//    Proprietary © Vicharak Computers PVT LTD - 2024
-//-----------------------------------------------------------------------------
+
 
 module usbls_tx_addr_endp_inv (
     input  [0:10] addr_endp,
