88b090fcb5fd628a04ab091af82542255a328362
Mahek-H-Varasada <mahekvarasada7@gmail.com>
2025-08-11 22:37:51 +0530

    Update usbls_tx_pid_gen.v

--- usbls_tx_pid_gen.v
+++ usbls_tx_pid_gen.v
@@ -1,28 +1,4 @@
-//-----------------------------------------------------------------------------
-// Company:         Vicharak Computers PVT LTD
-// Engineer:        Mahekkumar Varasada <mahekvarasada@gmail.com>
-// 
-// Create Date:     April 2, 2025
-// Design Name:     usbls_tx_pid_gen
-// Module Name:     usbls_tx_pid_gen.v
-// Project:         PeriPlex
-// Target Device:   Trion T120F324
-// Tool Versions:   Efinix Efinity 2024.2 
-// 
-// Description: 
-//    This module selects appropriate PID based on Input flags
-// 
-// Dependencies: 
-// 
-// Version:
-//    1.0 - 02/04/2025 - MKV - Initial release
-// 
-// Additional Comments: 
-//    
-// 
-// License: 
-//    Proprietary © Vicharak Computers PVT LTD - 2024
-//-----------------------------------------------------------------------------
+
 module usbls_tx_pid_gen(
 	input              crc_c,
     input              set_report,
